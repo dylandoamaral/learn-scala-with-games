7be72df6409978ca0cb062fbec74a3b69975114f
dylandoamaral <do.amaral.dylan@gmail.com>
2020-03-02 18:02:24 +0100

    add: life print

--- src/main/scala/life/Main.scala
+++ src/main/scala/life/Main.scala
@@ -1,5 +1,31 @@
 package life
 
+import life.Life.{Alive, Dead, GameState}
+
 object Main extends App {
-  println("We start with an impure function ¯\\_(ツ)_/¯")
+  def printGameState(state: GameState): Unit = {
+    state.board.foreach(
+      line => {
+        line.foreach {
+          case Dead => print("0")
+          case _    => print("1")
+        }
+        println()
+      }
+    )
+
+    Thread.sleep(1000)
+    printGameState(Life.next(state))
+  }
+
+  val state = GameState(
+    Array(
+      Array(Dead, Dead, Dead),
+      Array(Alive, Alive, Alive),
+      Array(Dead, Dead, Dead)
+    ), (3, 3)
+  )
+
+
+  printGameState(state)
 }
