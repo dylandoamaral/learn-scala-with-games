825d57e74f8f9ab4765ceb9aea8d6f42c2626e95
dylandoamaral <do.amaral.dylan@gmail.com>
2020-03-04 18:15:31 +0100

    add: life key input

--- build.sbt
+++ build.sbt
@@ -4,5 +4,6 @@ version := "0.1"
 
 libraryDependencies += "org.scalactic" %% "scalactic" % "3.1.1"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.1.1" % "test"
+libraryDependencies += "org.jline"      % "jline-terminal" % "3.12.1"
 
 scalaVersion := "2.13.1"
--- src/main/scala/life/Main.scala
+++ src/main/scala/life/Main.scala
@@ -1,25 +1,11 @@
 package life
 
+import java.io.IOException
+
 import life.Life.GameState
+import org.jline.terminal.TerminalBuilder
 
 object Main extends App {
-  def printGameState(state: GameState): Unit = {
-    println("\u001b[2J")
-    state.board.foreach(
-      line => {
-        line.foreach {
-          case 0 => print("0")
-          case _    => print("1")
-        }
-        println()
-      }
-    )
-
-    Thread.sleep(1000)
-
-    printGameState(Life.next(state))
-  }
-
   val blinker = GameState(
     Array(
       Array(0, 0, 0),
@@ -47,5 +33,41 @@ object Main extends App {
     ), (5, 5)
   )
 
-  printGameState(glider)
+  val structureMapping: Map[Int, GameState] = Map(97 -> blinker, 122 -> block, 101 -> glider)
+
+  @scala.annotation.tailrec
+  def printGameState(state: GameState): Unit = {
+    println("\u001b[2J")
+    state.board.foreach(
+      line => {
+        line.foreach {
+          case 0 => print("0")
+          case _    => print("1")
+        }
+        println()
+      }
+    )
+
+    Thread.sleep(1000)
+
+    getKeyInput match {
+      case Some(k) => printGameState(structureMapping(k))
+      case None => printGameState(Life.next(state))
+    }
+  }
+
+  val reader = TerminalBuilder.terminal().reader()
+
+  def getKeyInput: Option[Int] = {
+    try{
+      reader.read(1) match {
+        case v if structureMapping.exists(_._1 == v) => Some(v)
+        case _ => None
+      }
+    }catch{
+      case _: Throwable => None
+    }
+  }
+
+  printGameState(blinker)
 }
