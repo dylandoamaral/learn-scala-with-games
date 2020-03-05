1ef55476f9b3d0091147228dc075db6e26746156
dylandoamaral <do.amaral.dylan@gmail.com>
2020-03-05 10:15:45 +0100

    [life] add: doc

--- src/main/scala/life/Life.scala
+++ src/main/scala/life/Life.scala
@@ -3,6 +3,11 @@ package life
 object Life {
   case class GameState(board: Array[Array[Int]], size: (Int, Int));
 
+  /**
+    * Evaluate the next state of a current game state
+    * @param state The current game state
+    * @return The next game state
+    */
   def next(state: GameState): GameState =
     state.copy(board = state.board.zipWithIndex.map {
       case (xel, x) =>
@@ -17,15 +22,33 @@ object Life {
         }
     })
 
+  /**
+    * Get position around the pos cell
+    * @param state The current state
+    * @param pos The pos cell
+    * @return  The sequence of cell that are around the pos cell
+    */
   def getPosAround(state: GameState, pos: (Int, Int)): Seq[(Int, Int)] =
     for {
       x <- pos._1 - 1 to pos._1 + 1
       y <- pos._2 - 1 to pos._2 + 1 if (x != pos._1 || y != pos._2)
     } yield (x, y)
 
+  /**
+    * Count the number of cell alive around the pos cell
+    * @param state The current state
+    * @param pos The pos cell
+    * @return The number of cell alive around the pos cell
+    */
+  def countAliveAround(state: GameState, pos: (Int, Int)): Int =
+    getPosAround(state, pos).map(getCellType(state, _)).count(_ == 1)
 
-  def countAliveAround(state: GameState, pos: (Int, Int)): Int = getPosAround(state, pos).map(getCellType(state, _)).count(_ == 1)
-
+  /**
+    * Get the cell type for a particular position
+    * @param state The current state
+    * @param pos The position
+    * @return The cell type or zero if incorrect position
+    */
   def getCellType(state: GameState, pos: (Int, Int)): Int = pos match {
     case p
         if p._1 <= -1 | p._2 <= -1 | p._1 >= state.size._1 | p._2 >= state.size._2 =>
