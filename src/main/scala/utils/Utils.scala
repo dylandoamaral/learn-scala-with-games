a8782f513523d14c264dcde089caab1866a54424
dylandoamaral <do.amaral.dylan@gmail.com>
2020-03-06 10:08:08 +0100

    [life] fix: 2d array map

--- src/main/scala/utils/Utils.scala
+++ src/main/scala/utils/Utils.scala
@@ -1,7 +1,9 @@
 package utils
 
+import scala.reflect.ClassTag
+
 object Utils {
-  def map2dArray[A](l: Array[Array[A]], f: (Int, Int) => A): Array[Array[A]] = {
+  def map2dArray[A: ClassTag](l: Array[Array[A]], f: (Int, Int) => A): Array[Array[A]] = {
     l.zipWithIndex.map {
       case (xel, x) =>
         xel.zipWithIndex.map {
